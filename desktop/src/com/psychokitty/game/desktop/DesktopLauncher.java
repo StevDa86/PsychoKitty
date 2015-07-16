30154a382dc92679e8476f0af868eed107a7d49c
Steven <steven.danz@t-online.de>
2015-07-16 20:30:36 +0200

    My first Commit

---
+++ desktop/src/com/psychokitty/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.psychokitty.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.psychokitty.game.PsychoKittyGame;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PsychoKittyGame(), config);
+	}
+}
