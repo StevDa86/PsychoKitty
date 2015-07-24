065f21c50a10d0a3101e6023df113edc6f0b46d0
steven <steven.danz@t-onlne.de>
2015-07-24 23:37:08 +0200

    splash umbau

---
+++ core/src/com/psychokitty/game/Constants.java
@@ -0,0 +1,21 @@
+package com.psychokitty.game;
+
+/**
+ * Created by steven on 24.07.15.
+ */
+public class Constants {
+    //Values
+    public static final float catspeed = 200f;
+
+    //Images
+    public static final String backgroundImage = "Backgrounds/SC1BG.jpg";
+    public static final String foregroundImage = "Backgrounds/FGSC1.png";
+    public static final String playerImage = "Characters/cat.png";
+    public static final String splashImage = "Backgrounds/splash.jpg";
+    public static final String backgroundMenu = "";
+    public static final String catnipImage = "Characters/droplet.png";
+
+    //Sounds n Music
+    public static final String soundMiau = "Sounds/cat.mp3";
+    public static final String musicDream = "Music/dream.mp3";
+}
