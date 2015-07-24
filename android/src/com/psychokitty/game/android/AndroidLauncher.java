0b0ac7a8a35150270efaa9c19577d12a088e1f92
steven <steven.danz@t-onlne.de>
2015-07-24 20:01:09 +0200

    move char

--- android/src/com/psychokitty/game/android/AndroidLauncher.java
+++ android/src/com/psychokitty/game/android/AndroidLauncher.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.psychokitty.game.psychokittyGame;
+
+import com.psychokitty.game.PsychoKittyGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new psychokittyGame(), config);
+		initialize(new PsychoKittyGame(), config);
 	}
 }
