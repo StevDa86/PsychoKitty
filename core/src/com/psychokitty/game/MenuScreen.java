b0a9a80ce6ef6130f35aefb46b26cb604d6d3b44
steven <steven.danz@t-onlne.de>
2015-07-28 21:47:09 +0200

    buttons on menu

--- core/src/com/psychokitty/game/MenuScreen.java
+++ core/src/com/psychokitty/game/MenuScreen.java
@@ -4,25 +4,56 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
  * Created by steven on 24.07.15.
  */
 public class MenuScreen implements Screen {
-    private Texture texture = new Texture(Gdx.files.internal(Constants.backgroundMenu));
-    private Image splashImage = new Image(texture);
+
     private Stage stage = new Stage();
     private Music menuMusic = Gdx.audio.newMusic(Gdx.files.internal(Constants.musicMenu));
 
+    private Skin skin;
+
     final PsychoKittyGame game;
+
+    private void createBasicSkin(){
+        //Create a font
+        BitmapFont font = new BitmapFont();
+        skin = new Skin();
+        skin.add("default", font);
+
+        //Create a texture
+        Pixmap pixmap = new Pixmap((int)Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("background",new Texture(pixmap));
+
+        //Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+    }
 
     public MenuScreen(final PsychoKittyGame gam) {
         game = gam;
+
     }
 
     @Override
@@ -40,17 +71,42 @@ public class MenuScreen implements Screen {
     @Override
     public void show() {
         menuMusic.setVolume(1);
+        menuMusic.setLooping(true);
         menuMusic.play();
-        splashImage.setPosition(Gdx.graphics.getWidth() / 2 - splashImage.getWidth() / 2, Gdx.graphics.getHeight() / 2 - splashImage.getHeight() / 2);
-        stage.addActor(splashImage);
 
-        splashImage.addAction(Actions.sequence(Actions.alpha(0)
-                , Actions.fadeIn(1.0f), Actions.delay(2), Actions.run(new Runnable() {
-            @Override
-            public void run() {
+        Gdx.input.setInputProcessor(stage);// Make the stage consume events
+
+        createBasicSkin();
+        TextButton newGameButton = new TextButton("New game", skin); // Use the initialized skin
+        TextButton newDonateButton = new TextButton("Donate Paypal", skin);
+        TextButton newExitButton = new TextButton("Exit", skin);
+
+        newGameButton.setPosition(Gdx.graphics.getWidth()/2 - Gdx.graphics.getWidth()/8 , Gdx.graphics.getHeight()/2 + 100);
+        newDonateButton.setPosition(Gdx.graphics.getWidth()/2 - Gdx.graphics.getWidth()/8 , Gdx.graphics.getHeight()/2 );
+        newExitButton.setPosition(Gdx.graphics.getWidth()/2 - Gdx.graphics.getWidth()/8 , Gdx.graphics.getHeight()/2 - 100);
+
+        stage.addActor(newGameButton);
+        stage.addActor(newDonateButton);
+        stage.addActor(newExitButton);
+
+        newGameButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                // Do something interesting here...
                 ((Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen(game));
             }
-        })));
+        });
+        newDonateButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                // Do something interesting here...
+                Gdx.net.openURI("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=steven%2edanz%40t%2donline%2ede&lc=DE&item_name=Psycho%20Kitty&no_note=0&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest");
+            }
+        });
+        newExitButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                // Do something interesting here...
+                Gdx.app.exit();
+            }
+        });
     }
 
     @Override
@@ -68,7 +124,6 @@ public class MenuScreen implements Screen {
 
     @Override
     public void dispose() {
-        texture.dispose();
         stage.dispose();
         menuMusic.dispose();
     }
