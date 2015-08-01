6d538a9fb081f79fbadde3825027064d6a32f71e
steven <steven.danz@t-onlne.de>
2015-08-01 19:55:16 +0200

    Admob integration

--- android/src/com/psychokitty/game/android/AndroidLauncher.java
+++ android/src/com/psychokitty/game/android/AndroidLauncher.java
@@ -1,18 +1,77 @@
 package com.psychokitty.game.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdRequest;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+import com.psychokitty.game.AdsController;
 import com.psychokitty.game.PsychoKittyGame;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements AdsController{
+	private static final String BANNER_AD_UNIT_ID = "ca-app-pub-1866262097277557/3138629828";
+	AdView bannerAd;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new PsychoKittyGame(), config);
+		//initialize(new PsychoKittyGame(), config);
+		//hier neu wegen adview
+		View gameView = initializeForView(new PsychoKittyGame(this), config);
+		setupAds();
 
+		// Define the layout
+		RelativeLayout layout = new RelativeLayout(this);
+		layout.addView(gameView, ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		layout.addView(bannerAd, params);
+
+		setContentView(layout);
+
+	}
+
+	public void setupAds() {
+		bannerAd = new AdView(this);
+		bannerAd.setVisibility(View.INVISIBLE);
+		bannerAd.setBackgroundColor(0xff000000); // black
+		bannerAd.setAdUnitId(BANNER_AD_UNIT_ID);
+		bannerAd.setAdSize(AdSize.SMART_BANNER);
+	}
+
+	@Override
+	public void showBannerAd() {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				bannerAd.setVisibility(View.VISIBLE);
+				AdRequest.Builder builder = new AdRequest.Builder();
+				AdRequest ad = builder.build();
+				bannerAd.loadAd(ad);
+
+			}
+		});
+	}
+
+	@Override
+	public void hideBannerAd() {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				bannerAd.setVisibility(View.INVISIBLE);
+				AdRequest.Builder builder = new AdRequest.Builder();
+			}
+		});
 	}
 }
---
+++ core/src/com/psychokitty/game/AdsController.java
@@ -0,0 +1,10 @@
+package com.psychokitty.game;
+
+/**
+ * Created by steven on 01.08.15.
+ */
+public interface AdsController {
+    public void showBannerAd();
+
+    public void hideBannerAd();
+}
