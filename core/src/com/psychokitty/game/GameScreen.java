5c5d8e36bcb76c85169e263e9072340527d5bcf6
StevenD <steven.danz@004gmbh.de>
2015-09-25 10:49:53 +0200

    game over screen +  lives

--- core/src/com/psychokitty/game/GameScreen.java
+++ core/src/com/psychokitty/game/GameScreen.java
@@ -238,7 +238,6 @@ public class GameScreen implements Screen, InputProcessor {
                             GameOverState();
                         }
                     }
-
                 }
 
                 break;
