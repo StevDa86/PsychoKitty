49b38f426f8b6505abe3b39e9f123b8c9d83fb35
steven <steven.danz@t-onlne.de>
2019-02-01 22:13:15 +0100

    Asset Manager eingebaut

--- core/src/com/psychokitty/game/Utils/Assets.java
+++ core/src/com/psychokitty/game/Utils/Assets.java
@@ -3,19 +3,28 @@
 package com.psychokitty.game.Utils;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Assets {
 
     public static final AssetManager manager = new AssetManager();
 
+    //All Images
     public static final String Count1 = Constants.Number1Image;
     public static final String Count2 = Constants.Number2Image;
     public static final String Count3 = Constants.Number3Image;
-
     public static final String BackgroundImage = Constants.backgroundImage;
     public static final String ForegroundImage = Constants.foregroundImage;
 
+    //All Sounds
+    // load the drop sound effect and the rain background "music"
+    public static final String sSoundMiau = Constants.soundMiau;
+    public static final String sCatHiss = Constants.catHiss;
+    public static final String sMusicDream = Constants.musicDream;
+    public static final String sBeepHigh = Constants.beepHigh;
+    public static final String sBeepLow = Constants.beepLow;
 
     public static void load(){
         manager.load(Count1, Texture.class);
@@ -23,6 +32,12 @@ public class Assets {
         manager.load(Count3, Texture.class);
         manager.load(BackgroundImage, Texture.class);
         manager.load(ForegroundImage, Texture.class);
+        manager.load(sBeepHigh, Sound.class);
+        manager.load(sBeepLow, Sound.class);
+        manager.load(sCatHiss, Sound.class);
+        manager.load(sSoundMiau, Sound.class);
+        manager.load(sMusicDream, Music.class);
+        manager.finishLoading();
     }
 
     public static void dispose(){
