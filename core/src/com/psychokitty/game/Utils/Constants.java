1008873f9cafe4ac79f3a29949d1d460e6e66bfa
Cobain86 <steven.danz@t-online.de>
2019-12-08 15:53:11 +0100

    Update Menu Resolution and Button Images

--- core/src/com/psychokitty/game/Utils/Constants.java
+++ core/src/com/psychokitty/game/Utils/Constants.java
@@ -6,21 +6,30 @@ package com.psychokitty.game.Utils;
 public class Constants {
     //Values
     public static final float catspeed = 500f;
-    public static final int catsize = 150;
+    public static final int catsize    = 150;
+
+    //Spielauflösung
+    public static final int NATIVE_WIDTH  = 800;
+    public static final int NATIVE_HEIGHT = 480;
 
     //Images
-    public static final String backgroundImage = "Backgrounds/SC1BG.jpg";
-    public static final String foregroundImage = "Backgrounds/FGSC1.png";
-    public static final String playerImage = "Characters/cat.png";
-    public static final String splashImage = "Backgrounds/splash.png";
-    public static final String backgroundMenu = "Backgrounds/Menu.jpg";
-    public static final String catnipImage = "Characters/droplet.png";
+    public static final String backgroundImage  = "Backgrounds/SC1BG.jpg";
+    public static final String foregroundImage  = "Backgrounds/FGSC1.png";
+    public static final String playerImage      = "Characters/cat.png";
+    public static final String splashImage      = "Backgrounds/splash.png";
+    public static final String backgroundMenu   = "Backgrounds/Menu.jpg";
+    public static final String catnipImage      = "Characters/droplet.png";
     public static final String dogAnimationImage = "Characters/dogAnim.png";
-    public static final String heartImage = "ui/heart.png";
-    public static final String Number3Image = "ui/3.png";
-    public static final String Number2Image = "ui/2.png";
-    public static final String Number1Image = "ui/1.png";
-
+    public static final String heartImage       = "ui/heart.png";
+    public static final String Number3Image     = "ui/3.png";
+    public static final String Number2Image     = "ui/2.png";
+    public static final String Number1Image     = "ui/1.png";
+    public static final String startButton      = "Buttons/StartButton.png";
+    public static final String startButtonPressed      = "Buttons/StartButtonPressed.png";
+    public static final String scoreButton      = "Buttons/ScoreButton.png";
+    public static final String scoreButtonPressed      = "Buttons/ScoreButtonPressed.png";
+    public static final String exitButton      = "Buttons/ExitButton.png";
+    public static final String exitButtonPressed      = "Buttons/ExitButtonPressed.png";
     //Sounds n Music
     public static final String soundMiau = "Sounds/cat.mp3";
     public static final String catHiss = "Sounds/cat-hiss.mp3";
