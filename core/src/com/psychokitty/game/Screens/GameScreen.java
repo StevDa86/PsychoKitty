da9b1f8243b3a08e01d081f2e16c7a284ea85397
Cobain86 <steven.danz@t-online.de>
2023-09-26 16:00:21 +0200

    Upgrade to LibGDX 1.12.0 incl. all dependencies and tools

--- core/src/com/psychokitty/game/Screens/GameScreen.java
+++ core/src/com/psychokitty/game/Screens/GameScreen.java
@@ -485,6 +485,11 @@ public class GameScreen implements Screen, InputProcessor {
     }
 
     @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
--- core/src/com/psychokitty/game/Screens/SplashScreen.java
+++ core/src/com/psychokitty/game/Screens/SplashScreen.java
@@ -10,56 +10,58 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.psychokitty.game.PsychoKittyGame;
-
-/**
- * Created by steven on 24.07.15.
- */
+import com.psychokitty.game.Utils.Constants;
+import com.psychokitty.game.AdMob.AdsController;
 
 public class SplashScreen implements Screen {
 
-    private Texture texture = new Texture(Gdx.files.internal(com.psychokitty.game.Utils.Constants.splashImage));
-    private Image splashImage = new Image(texture);
-    private Stage stage = new Stage();
-    private Music splashSound = Gdx.audio.newMusic(Gdx.files.internal(com.psychokitty.game.Utils.Constants.soundSplash));
+    private final PsychoKittyGame game;
+    private final AdsController adcont;
+    private final Stage stage;
+    private final Music splashSound;
 
-    final PsychoKittyGame game;
-    public com.psychokitty.game.AdMob.AdsController adcont;
-
-
-    public SplashScreen(final PsychoKittyGame gam, com.psychokitty.game.AdMob.AdsController adsController) {
+    public SplashScreen(final PsychoKittyGame gam, AdsController adsController) {
         game = gam;
         adcont = adsController;
+        stage = new Stage();
+        splashSound = Gdx.audio.newMusic(Gdx.files.internal(Constants.soundSplash));
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
+        stage.act(delta);
         stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        // Handle resizing if needed.
     }
-
 
     @Override
     public void show() {
         splashSound.setVolume(1);
         splashSound.play();
 
-        splashImage.setPosition(Gdx.graphics.getWidth() / 2 - splashImage.getWidth() / 2, Gdx.graphics.getHeight() / 2 - splashImage.getHeight() / 2);
+        Texture texture = new Texture(Gdx.files.internal(Constants.splashImage));
+        Image splashImage = new Image(texture);
+
+        float imageX = Gdx.graphics.getWidth() / 2f - splashImage.getWidth() / 2f;
+        float imageY = Gdx.graphics.getHeight() / 2f - splashImage.getHeight() / 2f;
+
+        splashImage.setPosition(imageX, imageY);
         stage.addActor(splashImage);
 
-        splashImage.addAction(Actions.sequence(Actions.alpha(0), Actions.fadeIn(2.0f),
-                Actions.delay(3), Actions.run(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((Game) Gdx.app.getApplicationListener()).setScreen(new MenuScreen(game,adcont));
-                    }
-                })));
+        splashImage.addAction(
+                Actions.sequence(
+                        Actions.alpha(0),
+                        Actions.fadeIn(2.0f),
+                        Actions.delay(3),
+                        Actions.run(() -> ((Game) Gdx.app.getApplicationListener()).setScreen(new MenuScreen(game, adcont)))
+                )
+        );
     }
 
     @Override
@@ -69,17 +71,17 @@ public class SplashScreen implements Screen {
 
     @Override
     public void pause() {
+        // Handle pause if needed.
     }
 
     @Override
     public void resume() {
+        // Handle resume if needed.
     }
 
     @Override
     public void dispose() {
-        texture.dispose();
         stage.dispose();
         splashSound.dispose();
     }
-
 }
